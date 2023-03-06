1d138e668eab72714378ed1db073f8ee355d49da
Isheanesu Valentine Zvikaramba <zvkish001@myuct.ac.za>
2023-03-06 15:23:51 +0200

    Question 3.2
    In this question, a function has been created to count from 0 until my age has been reached and then the count restarts.

--- mec4126f_prac3_DVSCAM002/src/main.c
+++ mec4126f_prac3_DVSCAM002/src/main.c
@@ -7,6 +7,7 @@
 #define STM32F051												   //COMPULSORY
 #include "stm32f0xx.h"											   //COMPULSORY
 #include "lcd_stm32f0.h"
+#include <stdio.h>
 
 // GLOBAL VARIABLES ----------------------------------------------------------|
 struct age_data{
@@ -16,20 +17,33 @@ struct age_data{
 	uint8_t age;
 }my_age;
 
-cam
+
 
 // FUNCTION DECLARATIONS -----------------------------------------------------|
 
-void main(void);                                                   //COMPULSORY
+void main(void);
+void delay(unsigned int ms);//COMPULSORY
 
 // MAIN FUNCTION -------------------------------------------------------------|
 
 void main(void)
-{
+{	my_age.day=15;
+	my_age.month=2;
+	my_age.year=2000;
+	my_age.age=23;
+	init_LCD();
+	char lcd_str[1];
 
 
 	while(1)
 	{
+		for(uint8_t i=0; i<=my_age.age;i++){
+			delay(65000);
+			sprintf(lcd_str, "%d", i);
+			lcd_putstring(lcd_str);
+			delay(65000);
+			lcd_command(CLEAR);
+		}
 
 	}
 }
